15b191ef553a47d0066f9b7d8f7542305da55bbd
Paulo-Tonelotti <paulohenrique63664@gmail.com>
2023-03-27 14:51:12 -0300

    banco de dados memory

--- src/servidor.js
+++ src/servidor.js
@@ -1,2 +1,13 @@
 const porta = 3003
 
+const express = require('express')
+const app = express()
+
+
+app.get('/produtos', (req, res) => {
+    res.send({ nome: 'Notebook', preco: 123.45 })
+})
+
+app.listen(porta, () => {
+    console.log(`Servidor rodando na porta ${porta}`)
+})
