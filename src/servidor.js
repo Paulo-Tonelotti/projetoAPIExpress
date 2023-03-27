48eb1a871a7c86e974c9d2ca7dc9a37535b760b0
Paulo-Tonelotti <paulohenrique63664@gmail.com>
2023-03-27 15:22:11 -0300

    crud completo

--- src/servidor.js
+++ src/servidor.js
@@ -3,6 +3,10 @@ const porta = 3003
 const express = require('express')
 const app = express()
 const bancoDeDados =  require('./bancoDeDados')
+const bodyparser = require('body-parser')
+
+app.use(bodyparser.json())
+app.use(bodyparser.urlencoded({extended: true}))
 
 
 app.get('/produtos', (req, res) => {
@@ -15,13 +19,28 @@ app.get('/produtos/:id', (req, res, next) => {
     next()
 })
 
-app.post('/produtos', (req, res, next) => {
+app.post('/produtos/', (req, res, next) => {
     const produto = bancoDeDados.salvarProduto({
         nome: req.body.nome,
         preco: req.body.preco
     })
 
     res.send(produto).status(201)
+})
+
+app.put('/produtos/:id', (req, res, next) => {
+    const produto = bancoDeDados.salvarProduto({
+        id: req.params.id,
+        nome: req.body.nome,
+        preco: req.body.preco
+    })
+
+    res.send(produto).status(201)
+})
+
+app.delete('/produtos/:id', (req, res, next) => {
+    const produto = bancoDeDados.excluirProduto(req.params.id)
+    res.send(produto)
 })
 
 app.listen(porta, () => {
